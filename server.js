ea2b3754b3adf9ae7da1b40077f9f133691591fb
Amarachi Celine <ecelineamarachi@yahoo.com>
2023-06-13 00:37:15 +0100

    Created url search functionality

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS
     useUnifiedTopology: true
 })
 
-//set up the ejs folder
+//set up and connect the ejs folder
 app.set('view engine', 'ejs')
 
 //set app for url parameters
@@ -26,12 +26,64 @@ app.use(express.urlencoded({extended: false}))
 app.use(morgan('dev'));
 
 
-//creating the route endpoint 
+//creating the route endpoint
+//search functionality(get method) 
 app.get('/', async (req, res) => {
-   const scissorsUrl = await scissors.find()
-res.render('index', {scissorsUrl: scissorsUrl})
+//    const scissorsUrl = await scissors.find()
+// res.render('index', {scissorsUrl: scissorsUrl})
+
+const searchQuery = req.query.search || ''; // Extract the search query parameter or set it to an empty string if not provided
+
+const page = parseInt(req.query.page) || 1; // Get the page number from the query parameter, default to page 1
+
+// Calculate the starting and ending indexes for the URLs based on the page number and 10 URLs per page
+const perPage = 10;
+const startIndex = (page - 1) * perPage;
+const endIndex = page * perPage;
+
+try {
+  // Retrieve the URLs from the database or data source based on the calculated indexes
+
+  let query = {};
+
+  if (searchQuery) {
+    query = {
+      $or: [
+        { fullUrl: { $regex: searchQuery, $options: 'i' } },
+        { shortUrl: { $regex: searchQuery, $options: 'i' } }
+      ]
+    };
+  }
+
+  const totalCount = await scissors.countDocuments(query);
+
+  const allUrls = await scissors.find(query).skip(startIndex).limit(perPage);
+  const scissorsUrl = allUrls.slice(startIndex, endIndex);
+//   const scissorsUrl = await scissors.find().skip(startIndex).limit(perPage);
+
+
+// Filter the URLs based on the search query
+const filteredUrls = allUrls.filter(url => url.fullUrl.includes(searchQuery));
+
+
+// Pass the retrieved URLs and pagination information to the index.ejs template
+  res.render('index', { 
+    scissorsUrl: scissorsUrl, 
+    currentPage: page, 
+    totalPages: Math.ceil(allUrls.length / perPage),
+    searchQuery: searchQuery,
+});
+    
+}
+catch (err) {
+  // Handle any errors that occur during the retrieval
+  console.error('Error retrieving URLs:', err);
+  res.status(500).send("Internal Server Error"); // Internal Server Error
+}
 });
 
+
+//Update Full URLs
 app.post('/scissorsUrl', async(req, res) => {
 await scissors.create({ fullUrl: req.body.fullUrl})  //creating a new short url
 
